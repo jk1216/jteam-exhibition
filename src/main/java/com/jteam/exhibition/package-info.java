dc019691f68439f5c90b07a6ee46ea2685aa8b59
jk1216 <81555166@qq.com>
2017-03-19 22:39:19 +0800

    first commit

---
+++ src/main/java/com/jteam/exhibition/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Indicates that this module contains classes that need to be generated / processed.
+ */
+@ModuleGen(name = "jteam-exhibition-registration", groupPackage = "com.jteam.exhibition")
+package com.jteam.exhibition;
+
+import io.vertx.codegen.annotations.ModuleGen;
